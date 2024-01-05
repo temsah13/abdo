3b686aa5059570e4123df382640b6e5ffd569913
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-01-05 23:18:20 +0200

    change name

--- lib/tt.dart
+++ lib/tt.dart
@@ -6,7 +6,7 @@ class test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("abdo"),
+      child: Text("temsah sleem"),
     );
   }
 }
